6ac84ba65ac05cbd83acfa0a120ae4fc42091f18
jp-duminy <jp@duminy.org>
2026-02-20 16:37:32 +0000

    Made plots and names more accurate.

--- lightcurves.py
+++ lightcurves.py
@@ -13,8 +13,13 @@ import pandas as pd
 from astropy.time import Time
 from general_functions import Astro_Functions
 
+import scienceplots
+
+plt.style.use('science')
+plt.rcParams['text.usetex'] = False # this avoids an annoying latex installation
+
 cepheid_file = "/storage/teaching/TelescopeGroupProject/2025-26/student-work/Cepheids/Analysis/CalibratedData/cepheid_01_MW_Cyg.csv"
-reference_file = "/storage/teaching/TelescopeGroupProject/2025-26/student-work/Cepheids/Analysis/CalibratedData/references_cepheid_01_MW_Cyg.csv"
+reference_file = "/storage/teaching/TelescopeGroupProject/2025-26/student-work/Cepheids/Analysis/CalibratedData/references_01_MW_Cyg.csv"
 
 cep_df = pd.read_csv(cepheid_file)
 ref_df = pd.read_csv(reference_file)
@@ -24,14 +29,17 @@ ref_time_list = ref_df["ISOT"].dropna().astype(str).str.strip().tolist()
 
 class Lightcurves:
 
-    def __init__(self, name: str, time: float, m_stand, m_stand_err,
+    def __init__(self, name: str, cep_time: float, ref_time, m_stand, m_stand_err,
                  m_diff, m_diff_err, ref_df):
         """
         Initialise cepheid parameters relevant to period fitting.
         """
         self.name = name
-        self.mjd_time = Astro_Functions.modified_julian_date_converter(np.array(time)) # convert ISOT to MJD
-        self.time = self.mjd_time - self.mjd_time[0]
+        self.cep_mjd_time = Astro_Functions.modified_julian_date_converter(np.array(cep_time)) # convert ISOT to MJD
+        self.ref_mjd_time = Astro_Functions.modified_julian_date_converter(np.array(ref_time))
+
+        self.cep_time = self.cep_mjd_time - self.cep_mjd_time[0]
+        self.ref_time = self.ref_mjd_time - self.ref_mjd_time[0]
 
         self.m_stand = m_stand
         self.m_stand_err = m_stand_err
@@ -47,11 +55,11 @@ class Lightcurves:
         """
         fig, ax = plt.subplots()
         
-        ax.errorbar(self.mjd_time, self.m_stand, yerr=self.m_stand_err, fmt='o', 
+        ax.errorbar(self.cep_mjd_time, self.m_stand, yerr=self.m_stand_err, fmt='o', 
                     label=self.name, color='black', capsize=5)
         ax.set_xlabel('Time [MJD]')
-        ax.set_ylabel('Corrected Magnitude')
-        ax.set_title(f"Light Curve for Cepheid {self.name}")
+        ax.set_ylabel('Standard Magnitude [mag]')
+        ax.set_title(f"Light Curve for Cepheid {self.name} (Standard Magnitudes)")
         ax.legend()
         ax.invert_yaxis() # brighter -> lower magnitude
 
@@ -63,11 +71,11 @@ class Lightcurves:
         """
         fig, ax = plt.subplots()
         
-        ax.errorbar(self.mjd_time, self.m_diff, yerr=self.m_diff_err, fmt='o', 
+        ax.errorbar(self.cep_mjd_time, self.m_diff, yerr=self.m_diff_err, fmt='o', 
                     label=self.name, color='black', capsize=5)
         ax.set_xlabel('Time [MJD]')
-        ax.set_ylabel('Corrected Magnitude')
-        ax.set_title(f"Light Curve for Cepheid {self.name}")
+        ax.set_ylabel('Differential Magnitude [mag]')
+        ax.set_title(f"Light Curve for Cepheid {self.name} (Differential Magnitudes)")
         ax.legend()
         ax.invert_yaxis() # brighter -> lower magnitude
 
@@ -84,20 +92,20 @@ class Lightcurves:
         
         fig, ax = plt.subplots()
         for ref_id, group in self.ref_df.groupby("ref_id"):
-            mjd = Time(group["ISOT"].values, format="isot").mjd
-            ax.plot(mjd, group["offset"], 'o-', label=ref_id, markersize=4)
+            ax.plot(self.ref_mjd_time, group["offset"], 'o-', label=ref_id, markersize=4)
         ax.axhline(0, color='k', linestyle='--', alpha=0.3)
         ax.set_xlabel("MJD")
         ax.set_ylabel("Offset (m_cal - V_true) [mag]")
         ax.legend()
-        ax.set_title(f"Reference Star Stability — {self.name}")
+        ax.set_title(f"Reference Star Magnitudes — {self.name}")
         plt.show()
 
 def main():
 
     lc = Lightcurves(
     name="MW Cyg",
-    time=cep_time_list,
+    cep_time=cep_time_list,
+    ref_time=ref_time_list,
     m_stand=cep_df["m_standard"].values,
     m_stand_err=cep_df["m_standard_err"].values,
     m_diff=cep_df["m_differential"].values,
